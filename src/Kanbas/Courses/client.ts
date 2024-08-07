65e0fa1ae7fa5954918fe22c9c64a0f5251a419c
Neil Israni <114356760+NeilIsrani@users.noreply.github.com>
2024-08-06 21:57:54 -0400

    Update client.ts

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const REMOTE_SERVER = "http://localhost:4000";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER ;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 export const createCourse = async (course: any) => {
     const response = await axios.post(COURSES_API, course);
